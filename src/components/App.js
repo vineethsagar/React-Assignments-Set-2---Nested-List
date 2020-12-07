5b049410af8e7048c0c5f99c02f38196f4edf0f8
vineethsagar <30525698+vineethsagar@users.noreply.github.com>
2020-12-07 20:27:50 +0530

    modified completely

--- src/components/App.js
+++ src/components/App.js
@@ -155,12 +155,123 @@ const states = [
   }
 ];
 
-function App() {
+/* function App() {
   return (
     <div id="main">
       {states.map((state, index) => {
         return <State key={index + 1} id={index + 1} state={state} />;
       })}
+    </div>
+  );
+}
+
+export default App;
+ */
+
+function App() {
+  let [cities, setCities] = useState([]);
+  let [towns, setTowns] = useState([]);
+  const handleCity = (index) => {
+    let obj = undefined;
+    for (let i = 0; i < states.length; i++) {
+      if (i === index) {
+        obj = states[i]["cities"];
+        break;
+      }
+    }
+    let arr = [];
+    for (let i = 0; i < obj.length; i++) {
+      arr.push(obj[i]["name"]);
+    }
+    obj = cities;
+    let found = true;
+    for (let i = 0; i < Math.min(obj.length, arr.length); i++) {
+      if (obj[i] !== arr[i]) {
+        found = false;
+        break;
+      }
+    }
+    if (found && obj.length !== 0) {
+      setCities([]);
+      return;
+    }
+    setCities(arr);
+  };
+
+  const handleTown = (city) => {
+    let obj = undefined;
+    let found = false;
+    for (let i = 0; i < states.length; i++) {
+      let arr = states[i]["cities"];
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j]["name"] === city) {
+          obj = arr[j]["towns"];
+          found = true;
+          break;
+        }
+      }
+      if (found) {
+        break;
+      }
+    }
+    let arr = [];
+    for (let i = 0; i < obj.length; i++) {
+      arr.push(obj[i]["name"]);
+    }
+    obj = towns;
+    found = true;
+    for (let i = 0; i < Math.min(obj.length, arr.length); i++) {
+      if (obj[i] !== arr[i]) {
+        found = false;
+        break;
+      }
+    }
+    if (found && towns.length !== 0) {
+      setTowns([]);
+      return;
+    }
+    setTowns(arr);
+  };
+  return (
+    <div id="main">
+      <h1>states</h1>
+      <div>
+        {states.map((place, index) => {
+          return (
+            <button
+              key={`${place}state`}
+              id={`state${index + 1}`}
+              onClick={() => handleCity(index)}
+            >
+              {place["name"]}
+            </button>
+          );
+        })}
+      </div>
+      <h1>cities</h1>
+      <div>
+        {cities.map((place, index) => {
+          return (
+            <button
+              key={`${place}city`}
+              id={`city${index + 1}`}
+              onClick={() => handleTown(place)}
+            >
+              {place}
+            </button>
+          );
+        })}
+      </div>
+      <h1>towns</h1>
+      <div>
+        {towns.map((place, index) => {
+          return (
+            <div key={`${place}town`} id={`town${index + 1}`}>
+              {place}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
